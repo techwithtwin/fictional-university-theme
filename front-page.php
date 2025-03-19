fdc56e646ff95e6aa0673f6b14de035d41565951
techwithtwin <realwebtechguru@gmail.com>
2025-03-20 01:37:25 +0300

    adding custom fields

--- front-page.php
+++ front-page.php
@@ -25,41 +25,36 @@ get_header();
             <h2 class="headline headline--small-plus t-center">
                 Upcoming Events
             </h2>
+            <?php
+            $homeEvents = new WP_Query(array(
+                'posts_per_page' => 2,
+                'post_type' => 'event'
+            ));
 
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny">
-                        <a href="#">Poetry in the 100</a>
-                    </h5>
-                    <p>
-                        Bring poems you&rsquo;ve wrote to the 100 building this Tuesday
-                        for an open mic and snacks.
-                        <a href="#" class="nu gray">Learn more</a>
-                    </p>
+            while ($homeEvents->have_posts()) {
+                $homeEvents->the_post() ?>
+
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="#">
+                        <span class="event-summary__month"><?php the_time('M') ?></span>
+                        <span class="event-summary__day"><?php the_time('d') ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny">
+                            <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                        </h5>
+                        <p>
+                            <?php echo wp_trim_words(get_the_content(), 18) ?>
+                            <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Apr</span>
-                    <span class="event-summary__day">02</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny">
-                        <a href="#">Quad Picnic Party</a>
-                    </h5>
-                    <p>
-                        Live music, a taco truck and more can found in our third annual
-                        quad picnic day. <a href="#" class="nu gray">Learn more</a>
-                    </p>
-                </div>
-            </div>
+            <?php }
+            ?>
+
 
             <p class="t-center no-margin">
-                <a href="#" class="btn btn--blue">View All Events</a>
+                <a href="<?php echo site_url('/event') ?>" class="btn btn--blue">View All Events</a>
             </p>
         </div>
     </div>
@@ -86,7 +81,10 @@ get_header();
                             <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
                         </h5>
                         <p>
-                            <?php echo wp_trim_words(get_the_content(), 18) ?>
+                            <?php
+                            if (has_excerpt()) the_excerpt();
+                            else echo wp_trim_words(get_the_content(), 18)
+                            ?>
                             <a href="<?php the_permalink() ?>" class="nu gray">Read more</a>
                         </p>
                     </div>
--- functions.php
+++ functions.php
@@ -13,6 +13,8 @@ function university_features()
 {
     add_theme_support('title-tag');
 }
+
+
 add_action('wp_enqueue_scripts', 'university_files');
 
 add_action('after_setup_theme', 'university_features');
