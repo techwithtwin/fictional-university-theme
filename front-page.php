84170e3ca197b042eeb622cf2e7427d508836030
techwithtwin <realwebtechguru@gmail.com>
2025-03-20 00:26:08 +0300

    making a nav item light up when it is active

--- front-page.php
+++ front-page.php
@@ -96,10 +96,8 @@ get_header();
             wp_reset_postdata();
 
             ?>
-
-
             <p class="t-center no-margin">
-                <a href="#" class="btn btn--yellow">View All Blog Posts</a>
+                <a href="<?php echo site_url('/blog') ?>" class="btn btn--yellow">View All Blog Posts</a>
             </p>
         </div>
     </div>
